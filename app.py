38e28cf2d58e7b18afe9025d831f57fc0cac7091
Yukta <yukta@priyaqubit.onmicrosoft.com>
2025-05-06 18:56:45 +0530

    setting up ci/cd

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, jsonify,redirect, url_for,flash
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.secret_key = "secret_key"
+
+@app.route('/')
+def index():
+    return render_template('setup.html')
+
+if __name__ == '__main__':
+    # app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='8080')
